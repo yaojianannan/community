2198e08aee03ddeb0b26c60aa3a65d7fdd025b12
YaoJN <yaojn@hollycrm.com>
2019-07-17 11:09:57 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ https://spring.io/guides/ ---->spring文档
 https://spring.io/guides/gs/serving-web-content/
 http://elasticsearch.cn/explore---->社区
 https://github.com/yaojianannan/community/settings/keys----->创建ssh key
-
+https://v3.bootcss.com-----前端框架BootStarp
 ##工具
 https://git-scm.com/download
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by BJB139 on 2019/7/14.
  */
 @Controller
-public class HelloController {
-    @GetMapping
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
     }
 }
