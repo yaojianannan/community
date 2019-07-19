49bb4f47078e8504a9958e3d726e815440484616
YaoJN <yaojn@hollycrm.com>
2019-07-19 14:54:50 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 [Github Oauth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 [okhttp](https://square.github.io/okhttp/)
 [fastjson](https://mvnrepository.com/artifact/com.alibaba/fastjson)
+[mvn存储库](https://mvnrepository.com)[fastjson]
 ##工具
 [git](https://git-scm.com/download)
 [vp](https://www.visual-paradigm,com)
--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.dto.GithubUser;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-
+    @Value("${github.client.id}")
+    private  String clientid;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,@RequestParam(name = "state")String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("2218d041f360837def01");
-        accessTokenDTO.setClient_secret("7502b02721afcbc29a3877d3b5c70bf39c898789");
+        accessTokenDTO.setClient_id(clientid);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.GetAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
